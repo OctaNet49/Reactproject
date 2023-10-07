5044c370d6d1dd23e770a107e3604520d7cb4b69
Website-develp <abdulgajani79@gmail.com>
2023-10-07 10:54:45 -0700

    meet my cv

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+<div className='container-fluid'>
+
+<div className='row '>
+<div className='col-lg-5 bg-success'>
+
+<div className='container-fluid'>
+  
+  <div className='row '>
+  <div className='col-lg-12 bg-success'>
+  
+  <img src='./profile-pic.png' className='img-fluidn w-25 '></img>
+  
+  <ul className="list-group my-5 text-center">
+  <li className="list-group bg-secondary my-1"><a className="nav-link  text-white " aria-current="page" href="#">Home</a></li>
+  <li className="list-group bg-secondary my-1"><a className="nav-link  text-white " aria-current="page" href="#">Education</a></li>
+  <li className="list-group bg-secondary my-1 "><a className="nav-link text-white " aria-current="page" href="#">Skills</a></li>
+</ul>
+<div className="container my-5 text-white">
+    <h1 class="my-5 text-center bg-secondary">References</h1>
+    <h5 className='bg-warning shadow p-3'>  <i className="fa-solid fa-location-dot "></i>  Quaid-e-awam University of Engineering science and technology Nawabshah</h5>
+    <h5 className='bg-warning shadow p-3'> <i className="fa-solid fa-phone p-2"></i>03196459910</h5>
+    <h5 className='bg-warning shadow p-3'> <i className="fa-solid fa-envelope"></i> abdulgajani79@gmail.com</h5>
+    
+    <i className="fa-brands fa-linkedin p-2"></i>  <a href=" https://www.linkedin.com/in/abdul-kareem-a90a94292/" class="text-decoration-none text-white">Linkedin</a>
+    <i className=" fa-brands fa-github p-2"></i><a href="https://github.com/dashboard" className="text-decoration-none text-white">Github</a>
+    <h1 className='my-5'>Front-end Projects</h1>
+        <ul>
+          <li>Amazon Clone</li>
+          <li>E-commerce Websites</li>
+          <li>School and universities Websites</li>
+          <li>Guess the Number game using with the help of java script</li>
+          <li>Grading System using java script</li>
+          <li>A website with the great Animation effects and loading features.</li>
+          <l1>Netflex clone</l1>
+          <l1>Amazon clone</l1>
+        </ul>
+        <h1 className='my-3'>Back-end Project</h1>
+        <ul>
+        <h3>  Online book store </h3>
+        </ul>
+ </div>
+</div>
+ 
+</div>
+</div>
+</div>
+<div className='col-lg-7 bg-light'>
+<div className="container shadow ">
+<h1 className="my-3">Abdul Kareem</h1>
+        <h4>WEB DEVELOPER</h4>
+        <div className="container-fluid " id="About">
+            <div className="col-lg-12">
+             | Front-end Developer 💻 | back-end Developer Engineer 🌐
+            <br />
+
+Hello, i m  a passionate web developer with a knack for turning ideas into reality through elegant and efficient code. 💡 I specialize in both front-end and back-end technologies, creating seamless digital experiences that users love. My expertise includes:
+<br />
+Front-end Development: 🎨 HTML, CSS, JavaScript, React.js.<br />
+Back-end Development: 🛠️ Node.js, Express.js. <br />
+Databases: 📊 MongoDB, MySQL. <br />
+Version Control: 🔗 Git, GitHub <br />
+Tools: 🛠️ VS Code.  <br />
+I am dedicated to crafting visually appealing and intuitive websites and Webapps.  I am thrive on  tackling challenges, and delivering innovative solutions. 💪
+
+Let's connect and explore exciting opportunities to collaborate and create something amazing together! Feel free to reach out for web development projects, tech discussions, or just to say hi! 🚀
+
+Let's Code the Future Together! 💻✨
+            </div>
+            <div>
+                <i className="fa-solid fa-user-graduate"></i>
+                <i className="fa-sharp fa-solid fa-head-side-gear " > <h6 className="bg-secondary my-5 d-block p-3 m-3">WORK EXPERIENCE </h6></i>
+        <ol>
+            <li className="fs-bold bg-warning  d-block p-3 m-3 shadow"> Web-development fundamentals(Udemy)</li>
+          <p className="text-warning">  2021 </p>
+- Proficient in applying Web techniques to real-world problems.
+        
+        <li className="fs-bold my-2 bg-warning  d-block p-3 m-3 shadow">JavaScript (Udemy)</li>
+        <p className="text-warning">  2023 — Present </p>
+        Equipped with the skills to extract valuable insights from complex js problems.
+
+        <li className="fs-bold my-2 bg-warning  d-block p-3 m-3 shadow ">Excel (Udemy)</li>
+        <p className="text-warning">  2023 — Present </p>
+        Skilled in creating impactful Excel sheets and grip on Ms-Project.
+
+        <li className="fs-bold  bg-warning my-3 d-block p-3 m-3 shadow ">SQL  (Udemy)</li>
+        <p className="text-warning">  2023 — Present </p>
+        Well-versed in SQL querying for data manipulation .
+        <li className="fs-bold  bg-warning my-3 d-block p-3 m-3 shadow ">MERN-Stack  (Nexus berry)</li>
+        <p className="text-warning">  2023 — Present </p>
+        Learning and exploring the new things and great projects And the teaching Staff is really Awesome .
+
+      </ol> 
+    </div> 
+ 
+    </div>
+    <div className="conatiner">
+            <i className="fa-solid fa-user-graduate"></i>
+            <i className="fa-sharp fa-solid fa-head-side-gear"> <h5 className="bg-secondary my-5 d-block p-3 m-3" id="">Education </h5></i>
+        
+            </div>
+
+            <ol className="timeline-list">
+              <li className="timeline-item  bg-warning my-3 d-block p-3 m-3 shadow">
+              Govt: Boys High Scholl Mori
+            </li>
+                <span className="text-warning">2015 - 2017</span>
+
+                <p className="timeline-text">
+                  
+                    2015 - 2017
+                    During my time at School of Thought from 2015 to 2017, I cultivated a strong foundation in critical thinking and creativity, setting the stage for my educational journey.
+                </p>
+             
+
+              <li className="timeline-item bg-warning my-3 d-block p-3 m-3 shadow">
+               
+                    Govt: boys higher secondary School Babrlio
+                 </li>
+
+                <span className="text-warning">2017 — 2019</span>
+
+                <p className="timeline-text">
+                    My intermediate education was
+                    not only solidified my academic prowess but also honed
+                         my interpersonal skills through diverse interactions with
+                         peers.
+                </p>
+             
+
+              <li className="timeline-item bg-warning my-3 d-block p-3 m-3 shadow">
+               
+                  Quest University Nawabshah
+                </li>
+
+                <span className="text-warning">2020 — Present</span>
+
+                <p className="timeline-text">
+                    Pursuing a Bachelor of Science in Software Engineering at Quest University Nawabshah, I've embraced the challenges of higher education and expanded my horizons within the dynamic world of technology.
+                </p>
+              
+            </ol> 
+    
+        
+            <div>
+        <i className="fa-solid fa-user-graduate"></i>
+       <i className="fa-sharp fa-solid fa-head-side-gear"> <h5 className="bg-secondary my-5 d-block p-3 m-3">Skills Experties </h5></i>
+       <div>
+       <div className="col-lg-10">
+        HTML
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-75" role="progressbar"  >75%</div>
+        </div>
+      
+        css
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-50" role="progressbar"  >50%</div>
+        </div>
+        JavaScript
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-75" role="progressbar"  >75%</div>
+        </div>
+        Node.js
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-25" role="progressbar"  >25%</div>
+        </div>
+        Express.js
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-75" role="progressbar"  >75%</div>
+        </div>
+        MongoDB
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-50" role="progressbar"  >50%</div>
+        </div>
+        Bootstraps
+       <div className="progress my-3">
+      
+       <div className="progress-bar w-100" role="progressbar"  >100%</div>
+        </div>
+        </div>
+       
+        
+</div> 
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+    </div>
+    
+  );
+}
+
+
+export default App;
